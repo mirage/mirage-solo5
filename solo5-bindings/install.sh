3be6f17b274a97323f574428a8728245e6315bd3
Dan Williams <djwillia@us.ibm.com>
2016-04-19 14:45:38 +0000

    make sure opam copies lib

--- solo5-bindings/install.sh
+++ solo5-bindings/install.sh
@@ -7,6 +7,5 @@ fi
 
 odir=$prefix/lib
 mkdir -p $odir/pkgconfig $odir/mirage-solo5
-cp mirage-xen-ocaml-bindings.pc $odir/pkgconfig/mirage-xen-ocaml-bindings.pc
 cp mirage-solo5.pc $odir/pkgconfig/mirage-solo5.pc
 cp libsolo5camlbindings.a $odir/mirage-solo5/
