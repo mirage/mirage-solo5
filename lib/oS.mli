d6a492835b58f585f61f380d3b7f343ca8ef6362
Mindy Preston <mindy.preston@docker.com>
2016-11-08 11:04:14 -0600

    try to pack everything up into an OS module

--- lib/oS.mli
+++ lib/oS.mli
@@ -1,18 +1,36 @@
-(*
- * Copyright (c) 2010 Anil Madhavapeddy <anil@recoil.org>
- *
- * Permission to use, copy, modify, and distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *)
+module Env : sig
+
+(** Unikernel environment interface. *)
+
+val argv: unit -> (string array) Lwt.t
+(** The command line arguments given to the unikernel. The first
+    element is the name of the unikernel binary. The following
+    elements are the arguments passed to the unikernel. *)
+
+end
+
+module Lifecycle : sig
+
+val await_shutdown_request :
+  ?can_poweroff:bool ->
+  ?can_reboot:bool ->
+  unit -> [`Poweroff | `Reboot] Lwt.t
+(** [await_shutdown_request ()] is thread that resolves when the domain is
+    asked to shut down.
+    The optional [poweroff] (default:[true]) and [reboot] (default:[false])
+    arguments can be used to indicate which features the caller wants to
+    advertise (however, you can still get a request for a mode you didn't claim
+    to support). *)
+
+end
+
+module MM : sig
+module Heap_pages : sig
+  val total: unit -> int
+  val used: unit -> int
+end
+end
+module Time : sig
 
 type +'a io = 'a Lwt.t
 
@@ -59,3 +77,4 @@ val with_timeout : int64 -> (unit -> 'a Lwt.t) -> 'a Lwt.t
     Lwt.pick [Lwt_unix.timeout d; f ()]
     ]}
 *)
+end
