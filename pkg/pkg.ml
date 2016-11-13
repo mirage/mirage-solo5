032647345edd2b964c7a986bf322c79c38f8fad0
Hannes Mehnert <hannes@mehnert.org>
2016-11-13 15:50:42 +0000

    indent

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -6,12 +6,13 @@ open Topkg
 open Ocb_stubblr_topkg
 
 let opams = [
-  Pkg.opam_file "opam" ~lint_deps_excluding:
-    (Some ["ocaml-freestanding"])
+  Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["ocaml-freestanding"])
 ]
 
 let () =
   Pkg.describe ~build:(Pkg.build ~cmd()) ~opams "mirage-solo5" @@ fun c ->
-    Ok [ Pkg.mllib "lib/oS.mllib" ;
-         Pkg.clib "lib/libmirage-solo5_bindings.clib";
-         Pkg.share_root ~dst:"pkgconfig/" "lib/bindings/mirage-solo5.pc" ]
+  Ok [
+    Pkg.mllib "lib/oS.mllib" ;
+    Pkg.clib "lib/libmirage-solo5_bindings.clib";
+    Pkg.share_root ~dst:"pkgconfig/" "lib/bindings/mirage-solo5.pc"
+  ]
