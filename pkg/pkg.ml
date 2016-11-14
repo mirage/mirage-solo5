c4da27628dd692703e0ef8ebfd3e34885bb98fdb
Martin Lucina <martin.lucina@docker.com>
2016-11-14 16:12:03 +0100

    Don't install dllmirage-solo5_bindings.so

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -9,10 +9,12 @@ let opams = [
   Pkg.opam_file "opam" ~lint_deps_excluding:(Some ["ocaml-freestanding"])
 ]
 
+let nowhere ?force ?built ?cond ?exts ?dst _ = Pkg.nothing
+
 let () =
   Pkg.describe ~build:(Pkg.build ~cmd()) ~opams "mirage-solo5" @@ fun c ->
   Ok [
     Pkg.mllib "lib/oS.mllib" ;
-    Pkg.clib "lib/libmirage-solo5_bindings.clib";
+    Pkg.clib ~dllfield:nowhere "lib/libmirage-solo5_bindings.clib";
     Pkg.share_root ~dst:"pkgconfig/" "lib/bindings/mirage-solo5.pc"
   ]
