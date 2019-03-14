80a549693f95431f877e06f024ac461488b38194
Clément Pascutto <clement@tarides.com>
2019-03-14 16:58:29 +0100

    Port to dune

--- myocamlbuild.ml
+++
@@ -1,9 +0,0 @@
-open Ocamlbuild_plugin
-
-let () =
-  dispatch begin fun hook ->
-    Ocb_stubblr.init hook ;
-    match hook with
-    | After_rules -> flag [ "ocamlmklib" ; "custom" ] & A "-custom"
-    | _ -> ()
-  end
