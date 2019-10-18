e68e353fb9bf144881a4d0142aa19a73fad0052a
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-10-18 23:54:14 +0200

    Specialize compilation of MirageOS.* as an implementation of mirage-os-shim

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -4,6 +4,8 @@ let () =
   dispatch begin fun hook ->
     Ocb_stubblr.init hook ;
     match hook with
-    | After_rules -> flag [ "ocamlmklib" ; "custom" ] & A "-custom"
+    | After_rules ->
+      flag [ "ocaml"; "compile"; "without_cmi" ] (S [ A "-intf-suffix"; A ".ml" ]) ;
+      flag [ "ocamlmklib" ; "custom" ] & A "-custom"
     | _ -> ()
   end
