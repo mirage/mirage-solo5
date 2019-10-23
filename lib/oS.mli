42cb5905a5227d755492295e6b3569decd5faa5d
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-10-23 19:11:42 +0200

    Externalize OS.Main.at_enter

--- lib/oS.mli
+++ lib/oS.mli
@@ -16,6 +16,7 @@ end
 module Main : sig
 val wait_for_work_on_handle : int64 -> unit Lwt.t
 val run : unit Lwt.t -> unit
+val at_exit : (unit -> unit Lwt.t) -> unit
 val at_enter : (unit -> unit Lwt.t) -> unit
 val at_enter_iter : (unit -> unit) -> unit
 val at_exit_iter  : (unit -> unit) -> unit
