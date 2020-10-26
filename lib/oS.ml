9024c545cab7ac9cc60a257995bd9268f4779187
Hannes Mehnert <hannes@mehnert.org>
2020-10-26 13:06:57 +0100

    OS.Memory: new memory statistics interface
    Provide a new OS.Memory statistics interface modeled on the stdlib's Gc stat
    type. All units are in the system word size.
    
    The implementation takes care to account for the fact that on Solo5 backends the
    heap is shared with the stack, and a malloc() allocation on the heap will fail
    if the heap would grow to within 1MB of the stack.
    
    Mark OS.MM deprecated
    
    From mirage-xen 38b5a0f831250c26e22f136a96474ee32d6c43c5
    
    Co-authored-by: Martin Lucina <martin@lucina.net>

--- lib/oS.ml
+++ lib/oS.ml
@@ -1,5 +1,6 @@
 module Lifecycle = Lifecycle
 module Main = Main
+module Memory = Memory
 module MM = MM
 module Time = Time
 module Solo5 = Solo5
--- lib/oS.mli
+++ lib/oS.mli
@@ -18,8 +18,29 @@ val wait_for_work_on_handle : int64 -> unit Lwt.t
 val run : unit Lwt.t -> unit
 end
 
+module Memory : sig
+
+  (** Memory management operations. *)
+
+  (** Memory allocation statistics. Units are the system word size, as used by
+      the OCaml stdlib Gc module. *)
+  type stat = {
+    heap_words : int;  (** total number of words allocatable on the heap. *)
+    live_words : int;  (** number of live (i.e. allocated) words on the heap. *)
+    stack_words : int; (** number of words in use by the program stack.
+                           This includes any space reserved by a stack guard. *)
+    free_words : int;  (** number of free (i.e. allocatable) words on the heap. *)
+  }
+
+  val quick_stat: unit -> stat
+  (** [quick_stat ()]  returns memory allocation statistics. This call is
+      computationally cheap, but the returned values may not be completely
+      accurate. *)
+end
+
 module MM : sig
   val malloc_metrics : tags:'a Metrics.Tags.t -> ('a, unit -> Metrics.Data.t) Metrics.src
+  [@@ocaml.deprecated "This function will be deprecated. Use {!Memory.quick_stat} instead."]
 end
 
 module Time : sig
